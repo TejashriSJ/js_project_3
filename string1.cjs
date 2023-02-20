9f3f6e092a53e33c6438c9e67cc69a9aff899d47
TejashriSJ <tejashri.j.23.2@mountblue.tech>
2023-02-20 09:56:59 +0530

    Update string1 file
    This will clear the bug related to charecter replacement

--- string1.cjs
+++ string1.cjs
@@ -1,7 +1,9 @@
 function string1(stringFormat){
     
-    let formatedString = stringFormat.replace(/[$,]/g,''); // g- global match 
-    let result = parseFloat(formatedString);
+    let formatedString1 = stringFormat.replace('$','');
+    let formatedString2 = formatedString1.replaceAll(',','') 
+   
+    let result = (formatedString2 * 1);
     if (result) {
         return result;
     }
